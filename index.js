b07a6120af1bc55260a0adceb1ec82d25ea22c0e
Saif <kotwal61969@gmail.com>
2018-08-19 23:14:50 +0530

    Handling callback errors

--- index.js
+++ index.js
@@ -20,10 +20,18 @@ request({
     url: `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`,
     json: true
 }, (error, response, body) => {
-    var location = {
-        add: body.results[0].formatted_address,
-        lat: body.results[0].geometry.location.lat,
-        lng: body.results[0].geometry.location.lng
-    };
-    console.log(location);
+    if (error) {
+        console.log(error);
+    } else if(body.status === 'ZERO_RESULTS') {
+        console.log('Unable to find the address');
+    } else if(body.status === 'OK') {
+        var location = {
+            add: body.results[0].formatted_address,
+            lat: body.results[0].geometry.location.lat,
+            lng: body.results[0].geometry.location.lng
+        };
+        console.log(location);
+    } else {
+        console.log('API KEY EXPIRED');
+    }
 });
