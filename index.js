7a6151ab83cddd28206d3feb43f74410ed14bb13
Saif <kotwal61969@gmail.com>
2018-08-20 18:54:38 +0530

    Add weather functionality

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var yargs = require('yargs');
+var request = require('request');
 var geocode = require('./geocode/geocode');
 
 var argv = yargs
@@ -19,5 +20,15 @@ geocode.geocodeAddress(argv.a, (e, results) => {
         console.log(e);
     } else {
         console.log(JSON.stringify(results, undefined, 4));
+        request({
+            url: `https://api.darksky.net/forecast/199089917f59f46e5ac7dab70eaa7abd/${results.lat},${results.lng}`,
+            json: true
+        }, (error, response, body) => {
+            if (!error && response.statusCode === 200) {
+                console.log(body.currently.temperature);
+            } else {
+                console.log('Unable to fetch weather.');
+            }
+        })
     }
 });
