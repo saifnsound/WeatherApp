8e2a5b4520cdeb36acb5e2d835e35baccc1232eb
Saif <kotwal61969@gmail.com>
2018-08-21 00:19:38 +0530

    Added Promises and Removed Callbacks

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 var yargs = require('yargs');
-var geocode = require('./geocode/geocode');
-var weather = require('./weather/weather');
+var axios = require('axios');
 
 var argv = yargs
     .options({
@@ -15,16 +14,39 @@ var argv = yargs
     .alias('help', 'h')
     .argv;
 
-geocode.geocodeAddress(argv.a, (e, results) => {
-    if (e) {
-        console.log(e);
+var encodedAddress = encodeURIComponent(argv.a);
+var geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodedAddress}`;
+
+axios.get(geocodeUrl).then((response) => {
+
+    if (response.data.status === 'ZERO_RESULTS') {
+        throw new Error('Unable to locate the address.');
+    } else if (response.data.status === 'OVER_QUERY_LIMIT') {
+        throw new Error('API Not working');
+    }
+
+    var results = {
+        lat: response.data.results[0].geometry.location.lat,
+        lng: response.data.results[0].geometry.location.lng
+    };
+    var weatherURL = `https://api.darksky.net/forecast/199089917f59f46e5ac7dab70eaa7abd/${results.lat},${results.lng}`;
+
+    return axios.get(weatherURL);
+
+}).then((response) => {
+
+    var temperature = {
+        temp: response.data.currently.temperature,
+        appTemp: response.data.currently.apparentTemperature
+    };
+
+    console.log(`The Weather outside is ${temperature.temp}, but it feels like ${temperature.appTemp}.`);
+
+}).catch((e) => {
+
+    if (e.code === 'ENOTFOUND') {
+        console.log('Unable to connect to API servers.');
     } else {
-        weather.getWeather(results, (e, results) => {
-            if(e) {
-                console.log(e);
-            } else {
-                console.log(`The Weather outside is ${results.temp}, but it feels like ${results.appTemp}.`);
-            }
-        });
+        console.log(e.message);
     }
 });
